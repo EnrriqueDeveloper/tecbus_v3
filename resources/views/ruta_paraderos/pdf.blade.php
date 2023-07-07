1dc0357d84ab15452d7480ca73a3151f2d7d5f51
AntonioGuzman2003 <antonio.guzman@tecsup.edu.pe>
2023-07-07 10:47:35 -0700

    actualizando reporte h actual3

--- resources/views/ruta_paraderos/pdf.blade.php
+++ resources/views/ruta_paraderos/pdf.blade.php
@@ -7,49 +7,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbkjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        integrity="sha384-iYQeCzEYFbkjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+    <style>
+        .date-time-container {
+            margin-top: -30px;
+        }
+
+        .date-time-container p {
+            margin-bottom: 5px;
+        }
+    </style>
 
 </head>
 
 <body>
-    <div class="flex">
-        <div>
-            <img src="img/admin.png" alt="" width="50px" height="50px">
+    <div class="container">
+        <img src="img/admin.png" alt="" width="50px" height="50px">
+        <div class="d-flex justify-content-end date-time-container">
+            <div class="text-end">
+                <p class="text-end">Fecha: {{ date('Y-m-d') }}</p>
+                <p class="text-end">Hora: {{ date('H:i:s') }}</p>
+            </div>
+            <br>
+            <div>
+                <h2 style="text-align: center">REPORTE RUTA PARADEROS</h2>
+            </div>
         </div>
+        <table class="table table-bordered">
+            <thead class="bg-gray-300 text-white">
+                <tr>
+                    <th class="px-4 py-2">ID</th>
+                    <th class="px-4 py-2">NUM RUTA</th>
+                    <th class="px-4 py-2">PARADEROS</th>
+                    <th class="px-4 py-2">HORA PARTIDA</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($ruta_paraderos as $rutaparadero)
+                <tr>
+                    <td class="text-center">{{ $rutaparadero->id }}</td>
+                    <td class="text-center">{{ $rutaparadero->num_ruta }}</td>
+                    <td class="text-center">{{ $rutaparadero->n_paradero }}</td>
+                    <td class="text-center">{{ $rutaparadero->hora_partida }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
-    <div>
-    <?php
-     date_default_timezone_set('America/Lima');
-     ?>
-    <h2 style="text-align: center">REPORTE RUTA PARADEROS</h2>
-    <p class="text-right">Fecha: {{ date('Y-m-d') }}</p>
-    <p class="text-right">Hora: {{ date('H:i:s') }}</p>
-    <div>
-    <table class="table table-bordered">
-<thead class="bg-gray-300 text-white">
-            <tr>
-                <th class="px-4 py-2">ID</th>
-                <th class="px-4 py-2">NUM RUTA</th>
-                <th class="px-4 py-2">PARADEROS</th>
-                <th class="px-4 py-2">HORA PARTIDA</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($ruta_paraderos as $rutaparadero)
-            <tr>
-                <td class="text-center">{{ $rutaparadero->id }}</td>
-                <td class="text-center">{{ $rutaparadero->num_ruta }}</td>
-                <td class="text-center">{{ $rutaparadero->n_paradero }}</td>
-                <td class="text-center">{{ $rutaparadero->hora_partida }}</td>
-            </tr>
-            @endforeach
-        </tbody>    </table>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-c9X5d40GlpO2JJT/7hxBmvR66zx8ghWED4Ts9WvS0x0abJQSmHepn2rUoTme3Q3y"
-    crossorigin="anonymous"></script>
+        integrity="sha384-c9X5d40GlpO2JJT/7hxBmvR66zx8ghWED4Ts9WvS0x0abJQSmHepn2rUoTme3Q3y"
+        crossorigin="anonymous"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-PlbI6q5oKsIfc8+HfYHnGr3nD1pnUIM47euAmHPRBSttlcZCzCbyy2d4mDCmO+7C" crossorigin="anonymous"></script>
+        integrity="sha384-PlbI6q5oKsIfc8+HfYHnGr3nD1pnUIM47euAmHPRBSttlcZCzCbyy2d4mDCmO+7C" crossorigin="anonymous"></script>
 </body>
 
 </html>
